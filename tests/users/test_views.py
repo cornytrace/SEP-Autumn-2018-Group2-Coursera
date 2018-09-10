1a0157892a78c35ae875281f4a5054a0afae4677
Marten Kenbeek <marten.knbk@gmail.com>
2018-09-10 16:49:55 +0200

    Added test for authorize workflow.

--- tests/users/test_views.py
+++ tests/users/test_views.py
@@ -1,5 +1,8 @@
+from urllib.parse import urlparse
+
 import pytest
 from django.urls import reverse
+from oauth2_provider.models import Application, Grant
 
 
 @pytest.mark.django_db
@@ -8,3 +11,24 @@ def test_login_template(client):
     assert "registration/login.html" in [
         t.name for t in response.templates if t.name is not None
     ], "did not use correct template"
+
+
+@pytest.mark.django_db
+def test_authorize(client, user):
+    application = Application.objects.get(name="DASH-IT Frontend")
+    application.redirect_uris = "http://localhost:8080/"
+    application.save()
+
+    client.force_login(user)
+    response = client.get(
+        reverse("oauth2_provider:authorize")
+        + "?client_id="
+        + application.client_id
+        + "&state=random_state_string&response_type=code"
+    )
+    assert response.status_code == 302, "failed to get url"
+    url = urlparse(response.url)
+    assert url.scheme == "http", "wrong scheme"
+    assert url.netloc == "localhost:8080", "wrong netloc"
+    assert url.path == "/", "wrong path"
+    assert "code" in url.query, "couldn't find 'code' in query"
