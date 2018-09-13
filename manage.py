421fe1e23752e900ce8b5e5d108257c65f02179d
Marten Kenbeek <marten.knbk@gmail.com>
2018-09-13 15:54:35 +0200

    Use django-dotenv.

--- manage.py
+++ manage.py
@@ -2,7 +2,11 @@
 import os
 import sys
 
+import dotenv
+
 if __name__ == "__main__":
+    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eit_dashboard.settings")
     try:
         from django.core.management import execute_from_command_line
