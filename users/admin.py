4c1c4f6a1e633d854966442e63b2dff1a44e57ba
Cornee Traas <corneetraas@hotmail.com>
2018-09-18 10:43:30 +0200

    Add role selection to dev panel

--- users/admin.py
+++ users/admin.py
@@ -7,7 +7,7 @@ from users.models import User
 
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
+        (None, {"fields": ("email", "password", "role")}),
         (_("Personal info"), {"fields": ("first_name", "last_name")}),
         (
             _("Permissions"),
@@ -26,8 +26,8 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
     )
-    list_display = ("email", "first_name", "last_name", "is_staff")
-    search_fields = ("email", "first_name", "last_name")
+    list_display = ("email", "first_name", "last_name", "role", "is_staff")
+    search_fields = ("email", "first_name", "last_name", "role")
     ordering = ("email",)
 
 
