8c9b7d0a571404570ce737f63c549abd2e92852f
Marten Kenbeek <marten.knbk@gmail.com>
2018-10-05 12:38:28 +0200

    Added ItemGrade model and course_item_grades view.

--- coursera/models/grades.py
+++ coursera/models/grades.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-__all__ = ["Grade"]
+__all__ = ["Grade", "ItemGrade"]
 
 
 class Grade(models.Model):
@@ -40,3 +40,40 @@ class Grade(models.Model):
         managed = False
         db_table = "course_grades_view"
         unique_together = ("course", "eitdigital_user", "timestamp")
+
+
+class ItemGrade(models.Model):
+    NOT_PASSED = "not passed"
+    PASSED = "passed"
+    VERIFIED_PASSED = "verified passed"
+
+    PASSING_STATES = (
+        (NOT_PASSED, "Not passed"),
+        (PASSED, "Passed"),
+        (VERIFIED_PASSED, "Verified passed"),
+    )
+
+    id = models.CharField(primary_key=True, max_length=50)
+    course = models.ForeignKey(
+        "Course", related_name="item_grades", on_delete=models.DO_NOTHING
+    )
+    eitdigital_user = models.ForeignKey(
+        "EITDigitalUser", related_name="item_grades", on_delete=models.DO_NOTHING
+    )
+    item = models.ForeignKey(
+        "Item", related_name="item_grades", on_delete=models.DO_NOTHING
+    )
+    timestamp = models.DateTimeField(db_column="course_item_grade_ts")
+    passing_state = models.CharField(
+        max_length=255,
+        choices=PASSING_STATES,
+        db_column="course_item_passing_state_desc",
+    )
+    overall = models.FloatField(db_column="course_item_grade_overall")
+    verified = models.FloatField(db_column="course_item_grade_verified")
+    pending = models.FloatField(db_column="course_item_grade_pending")
+
+    class Meta:
+        managed = False
+        db_table = "course_item_grades_view"
+        unique_together = ("course", "item", "eitdigital_user")
